e90e28cbcfd36ea1e35719f765a62ea1d3ad5573
Stuart Alldritt <s.k.alldritt@gmail.com>
2023-07-09 13:27:10 -0700

    Add skeleton android app

---
+++ android/app/src/main/java/dev/autom8ed/rgbsunglasses/MainActivity.java
@@ -0,0 +1,53 @@
+package dev.autom8ed.rgbsunglasses;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import dev.autom8ed.rgbsunglasses.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.i("Permission", "Permission grant request code: " + requestCode);
+
+        for (int i = 0; i < permissions.length; i++) {
+            Log.i("Permission", permissions[i] + ": " + ((grantResults[i] == PackageManager.PERMISSION_GRANTED) ? "Granted" : "Denied"));
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestPermissions(new String[]{Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT}, 1);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_bluetoothscan)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
---
+++ android/app/src/main/java/dev/autom8ed/rgbsunglasses/ui/animations/AnimationTypes.java
@@ -0,0 +1,7 @@
+package dev.autom8ed.rgbsunglasses.ui.animations;
+
+public enum AnimationTypes {
+    None,
+    ZigZag,
+    Text
+}
