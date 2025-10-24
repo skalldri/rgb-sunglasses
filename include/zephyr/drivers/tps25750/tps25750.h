6ebafe41772625e53d00603525a628eae28449bc
Stuart Alldritt <s.k.alldritt@gmail.com>
2025-10-24 06:46:36 +0000

    Full project compiles again!

--- include/zephyr/drivers/tps25750/tps25750.h
+++ include/zephyr/drivers/tps25750/tps25750.h
@@ -1,14 +1,19 @@
 #pragma once
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-int tps25750_dump(const struct device *dev);
+    int tps25750_dump(const struct device *dev);
 
-int tps25750_download_patch(const struct device *dev, const char* patch, uint32_t patchSize);
+    int tps25750_download_patch(const struct device *dev, const char *patch, uint32_t patchSize);
 
-int tps25750_clear_dead_battery(const struct device *dev);
+    int tps25750_clear_dead_battery(const struct device *dev);
+
+#if defined(CONFIG_TPS25750_INTERNAL_PATCH)
+    int tps25750_get_patch(const char **patch, size_t *patch_size);
+#endif
 
 #ifdef __cplusplus
 };
--- misc/tps25750/tps25750-config-compressed.h
+++ misc/tps25750/tps25750-config-compressed.h
@@ -1,5 +1,11 @@
-#pragma once
+#ifndef TPS25750_PATCH_H
+#define TPS25750_PATCH_H
 
-extern const char tps25750x_lowRegion_i2c_array_lz4[];
+#define TPS25750_PATCH_COMPRESSED_SIZE 9769
+#define TPS25750_PATCH_UNCOMPRESSED_SIZE 14592
 
-extern int gtps25750x_lowRegion_i2c_array_lz4Size;
+extern const char tps25750_patch[];
+extern const unsigned int g_tps25750_patch_CompressedSize;
+extern const unsigned int g_tps25750_patch_UncompressedSize;
+
+#endif // TPS25750_PATCH_H
