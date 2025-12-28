fef702fb4a72d59aaa015c19ef9028c913cb8106
Stuart Alldritt <s.k.alldritt@gmail.com>
2025-12-28 22:18:01 +0000

    Use special CPF value for colors

--- src/animations/text_animation.cpp
+++ src/animations/text_animation.cpp
@@ -14,7 +14,7 @@ BT_SVC_UUID_DEFINE(TextAnimation);
 constexpr size_t kNumStringSlots = 20;
 
 using StepTimeMs = BT_SVC_READ_WRITE_VAR_CHRC_DEFINE(TextAnimation, 0, uint32_t, 50);
-using Color = BT_SVC_READ_WRITE_VAR_CHRC_DEFINE(TextAnimation, 1, uint32_t, 0xFFFFFFFF);
+using Color = BT_SVC_READ_WRITE_VAR_CHRC_DEFINE(TextAnimation, 1, Color, 0xFFFFFFFF);
 using UpNext = BT_SVC_READ_WRITE_VAR_CHRC_DEFINE(TextAnimation, 2, uint32_t, 0);
 
 // Declare a bunch of read/write string instance
@@ -44,34 +44,33 @@ BT_SVC_READ_WRITE_STRING_CHRC_DEFINE(TextAnimation, 119, TextAnimation::kMaxMsgL
 BT_SVC_IS_ACTIVE_CHRC_DEFINE(TextAnimation);
 
 BT_GATT_SERVICE_DEFINE(text_anim_service,
-    BT_SVC_UUID_REFERENCE(TextAnimation),
-    BT_SVC_READ_WRITE_VAR_CHRC_REFERENCE(TextAnimation, 0, "Step Time Ms"),
-    BT_SVC_READ_WRITE_VAR_CHRC_REFERENCE(TextAnimation, 1, "Color"),
-    BT_SVC_READ_WRITE_VAR_CHRC_REFERENCE(TextAnimation, 2, "Up Next"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 100, "Slot 0"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 101, "Slot 1"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 102, "Slot 2"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 103, "Slot 3"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 104, "Slot 4"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 105, "Slot 5"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 106, "Slot 6"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 107, "Slot 7"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 108, "Slot 8"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 109, "Slot 9"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 110, "Slot 10"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 111, "Slot 11"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 112, "Slot 12"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 113, "Slot 13"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 114, "Slot 14"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 115, "Slot 15"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 116, "Slot 16"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 117, "Slot 17"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 118, "Slot 18"),
-    BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 119, "Slot 19"),
-    BT_SVC_IS_ACTIVE_CHRC_REFERENCE(TextAnimation),
-);
+                       BT_SVC_UUID_REFERENCE(TextAnimation),
+                       BT_SVC_READ_WRITE_VAR_CHRC_REFERENCE(TextAnimation, 0, "Step Time Ms"),
+                       BT_SVC_READ_WRITE_VAR_CHRC_REFERENCE(TextAnimation, 1, "Color"),
+                       BT_SVC_READ_WRITE_VAR_CHRC_REFERENCE(TextAnimation, 2, "Up Next"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 100, "Slot 0"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 101, "Slot 1"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 102, "Slot 2"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 103, "Slot 3"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 104, "Slot 4"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 105, "Slot 5"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 106, "Slot 6"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 107, "Slot 7"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 108, "Slot 8"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 109, "Slot 9"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 110, "Slot 10"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 111, "Slot 11"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 112, "Slot 12"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 113, "Slot 13"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 114, "Slot 14"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 115, "Slot 15"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 116, "Slot 16"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 117, "Slot 17"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 118, "Slot 18"),
+                       BT_SVC_READ_WRITE_STRING_CHRC_REFERENCE(TextAnimation, 119, "Slot 19"),
+                       BT_SVC_IS_ACTIVE_CHRC_REFERENCE(TextAnimation), );
 
-const char* kStaticMessages[kNumStringSlots] = {
+const char *kStaticMessages[kNumStringSlots] = {
     "LIFE IS MADE OF LITTLE MOMENTS LIKE THIS",
     "WE ARE ALL WE NEED",
     "SO LONG AND THANKS FOR ALL THE FISH",
@@ -94,34 +93,39 @@ const char* kStaticMessages[kNumStringSlots] = {
     "THE GORGE",
 };
 
-template<size_t tChrcId>
+template <size_t tChrcId>
 using StrSlot = BtReadWriteString<TextAnimation::kBtServiceIdNum, tChrcId, TextAnimation::kMaxMsgLen>;
 
 // Helper template to initialize everything
-template<size_t tChrcId>
-static void inline initStrSlot() {
-    StrSlot<tChrcId>::getInstance().setValue(kStaticMessages[tChrcId-kStringSlotStartChrc]);
-    initStrSlot<tChrcId-1>();
+template <size_t tChrcId>
+static void inline initStrSlot()
+{
+    StrSlot<tChrcId>::getInstance().setValue(kStaticMessages[tChrcId - kStringSlotStartChrc]);
+    initStrSlot<tChrcId - 1>();
 }
 
-template<>
-void inline initStrSlot<kStringSlotStartChrc>() {
+template <>
+void inline initStrSlot<kStringSlotStartChrc>()
+{
     StrSlot<kStringSlotStartChrc>::getInstance().setValue(kStaticMessages[0]);
 }
 
-TextAnimation::TextAnimation() {
+TextAnimation::TextAnimation()
+{
     initStrSlot<119>();
 }
 
-size_t TextAnimation::getUpNext() {
+size_t TextAnimation::getUpNext()
+{
     uint32_t currUpNext = UpNext::getInstance();
     uint32_t nextUpNext = currUpNext + 1;
-    if (nextUpNext >= kNumStringSlots) {
+    if (nextUpNext >= kNumStringSlots)
+    {
         nextUpNext = 0; // Wraparound
     }
 
     // LOG_INF("Playing %u now, %u up next", currUpNext, nextUpNext);
-    
+
     // Update the variable which will get reflected on the BT remote app, allowing the user
     // to change the next phrase if needed
     UpNext::getInstance() = nextUpNext;
@@ -130,42 +134,52 @@ size_t TextAnimation::getUpNext() {
 }
 
 // Helper template to initialize everything
-template<size_t tChrcId>
-inline const char* getStringFromSlotTemplate(size_t slot) {
-    if ((slot + kStringSlotStartChrc) == tChrcId) {
+template <size_t tChrcId>
+inline const char *getStringFromSlotTemplate(size_t slot)
+{
+    if ((slot + kStringSlotStartChrc) == tChrcId)
+    {
         return StrSlot<tChrcId>::getInstance();
     }
 
-    return getStringFromSlotTemplate<tChrcId-1>(slot);
+    return getStringFromSlotTemplate<tChrcId - 1>(slot);
 }
 
-template<>
-inline const char* getStringFromSlotTemplate<kStringSlotStartChrc>(size_t slot) {
-    if ((slot + kStringSlotStartChrc) == kStringSlotStartChrc) {
+template <>
+inline const char *getStringFromSlotTemplate<kStringSlotStartChrc>(size_t slot)
+{
+    if ((slot + kStringSlotStartChrc) == kStringSlotStartChrc)
+    {
         return StrSlot<kStringSlotStartChrc>::getInstance();
     }
 
     return "INVALID STRING SLOT";
 }
 
-const char* TextAnimation::getStringFromSlot(size_t slot) {
-    if (slot >= kNumStringSlots) {
+const char *TextAnimation::getStringFromSlot(size_t slot)
+{
+    if (slot >= kNumStringSlots)
+    {
         return "INVALID STRING SLOT";
     }
 
     return getStringFromSlotTemplate<119>(slot);
 }
 
-void TextAnimation::init() {
+void TextAnimation::init()
+{
     currentCycleTimeMs = 0;
     currentTextOffset = 0;
     strncpy(currentMessage, kStaticMessages[getUpNext()], kMaxMsgLen);
 }
 
-void TextAnimation::tick(const LedConfig* config, const size_t timeSinceLastTickMs, const size_t bufferId) {
+void TextAnimation::tick(const LedConfig *config, const size_t timeSinceLastTickMs, const size_t bufferId)
+{
     // Turn off all LEDs
-    for (size_t x = 0; x < config->displayWidth; x++) {
-        for (size_t y = 0; y < config->displayHeight; y++) {
+    for (size_t x = 0; x < config->displayWidth; x++)
+    {
+        for (size_t y = 0; y < config->displayHeight; y++)
+        {
             pattern_controller_set_pixel_in_framebuffer(config, x, y, bufferId, 0, 0, 0);
         }
     }
@@ -176,7 +190,7 @@ void TextAnimation::tick(const LedConfig* config, const size_t timeSinceLastTick
     // High level theory of operation:
     // We will consider the entire text string to be a single giant texture.
     // The width of the texture == strlen(str) * FontAtlas::atlasPixelWidthPerChar
-    // 
+    //
     // The texture will start at the right side of the display: we apply an offset to the texture
     // render location, and then slowly reduce this offset (and eventually make it negative) to cause
     // the texture to scroll across the display.
@@ -209,8 +223,9 @@ void TextAnimation::tick(const LedConfig* config, const size_t timeSinceLastTick
 
     // Until currentTextOffset goes negative, the first char to display will always be 0
     // When currentTextOffset is negative, the first character has fallen off the edge of the display
-    if (currentTextOffsetRelativeToDisplay < displayWindowLeftSide) {
-        // For every FontAtlas::atlasPixelWidthPerChar we are beyond the displayWindowLeftSide, 
+    if (currentTextOffsetRelativeToDisplay < displayWindowLeftSide)
+    {
+        // For every FontAtlas::atlasPixelWidthPerChar we are beyond the displayWindowLeftSide,
         // we can start one character later
         int32_t offsetRelativeToDisplayWindowLeftSide = currentTextOffsetRelativeToDisplay - displayWindowLeftSide;
 
@@ -218,15 +233,19 @@ void TextAnimation::tick(const LedConfig* config, const size_t timeSinceLastTick
         // Rely on integer division to round down
         int32_t charsToDrop = offsetRelativeToDisplayWindowLeftSide / (int32_t)(-FontAtlas::atlasPixelWidthPerChar);
 
-        if (charsToDrop < 0) {
+        if (charsToDrop < 0)
+        {
             LOG_ERR("Chars to drop is negative unexpectedly: %d %d %d", currentTextOffsetRelativeToDisplay, offsetRelativeToDisplayWindowLeftSide, charsToDrop);
-        } else {
+        }
+        else
+        {
             firstChar += charsToDrop;
         }
     }
 
     // If we have finished scrolling the current message, pick the next message
-    if (firstChar >= currentMessageLen) {
+    if (firstChar >= currentMessageLen)
+    {
         currentTextOffset = 0;
         strncpy(currentMessage, getStringFromSlot(getUpNext()), kMaxMsgLen);
         return;
@@ -235,15 +254,18 @@ void TextAnimation::tick(const LedConfig* config, const size_t timeSinceLastTick
     int32_t charWindowPos = 0;
 
     // This function gets called repeatedly to render to the display
-    auto lambda = [&](size_t x, size_t y, bool filled) {
-        int32_t realX =  x + charWindowPos;
+    auto lambda = [&](size_t x, size_t y, bool filled)
+    {
+        int32_t realX = x + charWindowPos;
 
-        if (realX < 0 || realX >= (int32_t)config->displayWidth) {
+        if (realX < 0 || realX >= (int32_t)config->displayWidth)
+        {
             // Bail early if this pixel is not on the display
             return;
         }
 
-        if (filled) {
+        if (filled)
+        {
             uint32_t color = Color::getInstance();
             uint8_t red = (color >> 16) & 0xFF;
             uint8_t green = (color >> 8) & 0xFF;
@@ -252,7 +274,8 @@ void TextAnimation::tick(const LedConfig* config, const size_t timeSinceLastTick
         }
     };
 
-    for (size_t i = firstChar; i < currentMessageLen; i++) {
+    for (size_t i = firstChar; i < currentMessageLen; i++)
+    {
         // Calculate the position of the current character within the virtual texture buffer
         const int32_t currentCharPos = i * FontAtlas::atlasPixelWidthPerChar;
 
@@ -260,12 +283,15 @@ void TextAnimation::tick(const LedConfig* config, const size_t timeSinceLastTick
         charWindowPos = currentTextOffsetRelativeToDisplay + currentCharPos;
 
         // If the character is within the virtual display buffer, lets render it
-        if (charWindowPos >= displayWindowLeftSide && charWindowPos < displayWindowRightSide) {
+        if (charWindowPos >= displayWindowLeftSide && charWindowPos < displayWindowRightSide)
+        {
             FontAtlas::getInstance()->PrintChar(currentMessage[i], lambda);
 
             // Debugging
             // printk("%c", currentMessage[i]);
-        } else if (charWindowPos > displayWindowRightSide) {
+        }
+        else if (charWindowPos > displayWindowRightSide)
+        {
             // Early optimization: if we have found a character which is off the edge of the right side of the display window,
             // we can stop iterating since no more chars will ever need to be rendered
             break;
@@ -277,7 +303,8 @@ void TextAnimation::tick(const LedConfig* config, const size_t timeSinceLastTick
     // Add the time to our counter
     currentCycleTimeMs += timeSinceLastTickMs;
 
-    if (currentCycleTimeMs > StepTimeMs::getInstance()) {
+    if (currentCycleTimeMs > StepTimeMs::getInstance())
+    {
         currentCycleTimeMs = 0;
         currentTextOffset--; // Move text one pixel to the left
     }
--- src/bluetooth/read_write_variable.h
+++ src/bluetooth/read_write_variable.h
@@ -6,80 +6,91 @@
 template <typename T, size_t tAnimationId, size_t tChrcId, uint8_t format, T def>
 class BtReadWriteVariable
 {
-    public:
+public:
     static constexpr uint8_t kCpfFormat = format;
 
     // Allow variable assignment
-    T& operator=(const T& other) {
+    T &operator=(const T &other)
+    {
         getInstance().storage_ = other;
         getInstance().btNotifyIfEnabled();
         return getInstance().storage_;
     }
 
     // Allow casting to our underlying instance
-    operator T() {
+    operator T()
+    {
         return getInstance().storage_;
     }
 
-    static BtReadWriteVariable<T, tAnimationId, tChrcId, format, def>& getInstance() {
+    static BtReadWriteVariable<T, tAnimationId, tChrcId, format, def> &getInstance()
+    {
         static BtReadWriteVariable<T, tAnimationId, tChrcId, format, def> inst;
         return inst;
     }
 
-    static ssize_t write(struct bt_conn *conn, const struct bt_gatt_attr *attr, 
-            const void *buf, uint16_t len, uint16_t offset, 
-            uint8_t flags) 
-    { 
-            if (offset) { 
-                //printk("Animation %d, Chrc %d: error, offset\n", tAnimationId, tChrcId);
-                return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET); 
-            } 
-            if (len > sizeof(T)) {
-                //printk("Animation %d, Chrc %d: error, too long: %d > %d\n", tAnimationId, tChrcId, len, sizeof(T));
-                return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN); 
-            } 
-            memcpy(&(getInstance().storage_), buf, len);
+    static ssize_t write(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                         const void *buf, uint16_t len, uint16_t offset,
+                         uint8_t flags)
+    {
+        if (offset)
+        {
+            // printk("Animation %d, Chrc %d: error, offset\n", tAnimationId, tChrcId);
+            return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
+        }
+        if (len > sizeof(T))
+        {
+            // printk("Animation %d, Chrc %d: error, too long: %d > %d\n", tAnimationId, tChrcId, len, sizeof(T));
+            return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
+        }
+        memcpy(&(getInstance().storage_), buf, len);
 
-            //printk("Animation %d, Chrc %d: value updated over BT to '%x'\n", tAnimationId, tChrcId, getInstance().storage_);
+        // printk("Animation %d, Chrc %d: value updated over BT to '%x'\n", tAnimationId, tChrcId, getInstance().storage_);
 
-            return len;
+        return len;
     }
 
-    static ssize_t read(struct bt_conn *conn, const struct bt_gatt_attr *attr, 
-             void *buf, uint16_t len, uint16_t offset) 
-    { 
-        //printk("Animation %d, Chrc %d: Reading value '%d'\n", tAnimationId, tChrcId, getInstance().storage_);
-        return bt_gatt_attr_read(conn, attr, buf, len, offset, &(getInstance().storage_), sizeof(getInstance().storage_)); 
+    static ssize_t read(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                        void *buf, uint16_t len, uint16_t offset)
+    {
+        // printk("Animation %d, Chrc %d: Reading value '%d'\n", tAnimationId, tChrcId, getInstance().storage_);
+        return bt_gatt_attr_read(conn, attr, buf, len, offset, &(getInstance().storage_), sizeof(getInstance().storage_));
     }
 
     static void isActiveCccCfgChanged(const struct bt_gatt_attr *attr, uint16_t value)
     {
         getInstance().sendActiveNotifications_ = (value == BT_GATT_CCC_NOTIFY);
-        //printk("Anim %d chrc %d notification state: %d\n", tAnimationId, tChrcId, getInstance().sendActiveNotifications_);
+        // printk("Anim %d chrc %d notification state: %d\n", tAnimationId, tChrcId, getInstance().sendActiveNotifications_);
 
-        if (getInstance().sendActiveNotifications_) {
+        if (getInstance().sendActiveNotifications_)
+        {
             getInstance().activeAttr_ = attr;
-        } else {
+        }
+        else
+        {
             getInstance().activeAttr_ = NULL;
         }
     }
 
-    protected:
-        static void btNotifyIfEnabled() {
-            if (getInstance().activeAttr_) {
-                //printk("Anim %d chrc %d notifying on value change\n", tAnimationId, tChrcId);
-                if (bt_gatt_notify(NULL, getInstance().activeAttr_, &getInstance().storage_, sizeof(getInstance().storage_)) != 0) {
-                    //printk("Anim %d chrc %d notify failed\n", tAnimationId, tChrcId);
-                }
+protected:
+    static void btNotifyIfEnabled()
+    {
+        if (getInstance().activeAttr_)
+        {
+            // printk("Anim %d chrc %d notifying on value change\n", tAnimationId, tChrcId);
+            if (bt_gatt_notify(NULL, getInstance().activeAttr_, &getInstance().storage_, sizeof(getInstance().storage_)) != 0)
+            {
+                // printk("Anim %d chrc %d notify failed\n", tAnimationId, tChrcId);
             }
         }
+    }
 
-        BtReadWriteVariable() = default;
+    BtReadWriteVariable() = default;
 
-        T storage_ = def;
+    T storage_ = def;
 
-        bool sendActiveNotifications_ = false;
-        const struct bt_gatt_attr *activeAttr_ = NULL;
+    bool sendActiveNotifications_ = false;
+    const struct bt_gatt_attr *activeAttr_ = NULL;
 };
 
 template <size_t tAnimationId, size_t tChrcId, bool def = false>
@@ -103,17 +114,19 @@ using BtReadWrite_int16_t = BtReadWriteVariable<int16_t, tAnimationId, tChrcId, 
 template <size_t tAnimationId, size_t tChrcId, int32_t def = 0>
 using BtReadWrite_int32_t = BtReadWriteVariable<int32_t, tAnimationId, tChrcId, BLE_GATT_CPF_FORMAT_SINT32, def>;
 
+template <size_t tAnimationId, size_t tChrcId, uint32_t def = 0>
+using BtReadWrite_Color = BtReadWriteVariable<uint32_t, tAnimationId, tChrcId, BLE_GATT_CPF_FORMAT_RGB888, def>;
 
-#define BT_SVC_READ_WRITE_VAR_CHRC_DEFINE(_bt_service_class, _char_num, _type, _initial_value) \
-    BtReadWrite_ ## _type<_bt_service_class::kBtServiceIdNum, _char_num, _initial_value>; \
-    using _bt_service_class ## _char_num ## _ReadWrite = BtReadWrite_ ## _type<_bt_service_class::kBtServiceIdNum, _char_num, _initial_value>; \
-    BT_SVC_CHRC_DEFINE(read_write_ ## _bt_service_class ## _char_num, _bt_service_class::kBtServiceIdNum, _char_num, _bt_service_class ## _char_num ## _ReadWrite::kCpfFormat);
+#define BT_SVC_READ_WRITE_VAR_CHRC_DEFINE(_bt_service_class, _char_num, _type, _initial_value)                                           \
+    BtReadWrite_##_type<_bt_service_class::kBtServiceIdNum, _char_num, _initial_value>;                                                  \
+    using _bt_service_class##_char_num##_ReadWrite = BtReadWrite_##_type<_bt_service_class::kBtServiceIdNum, _char_num, _initial_value>; \
+    BT_SVC_CHRC_DEFINE(read_write_##_bt_service_class##_char_num, _bt_service_class::kBtServiceIdNum, _char_num, _bt_service_class##_char_num##_ReadWrite::kCpfFormat);
 
 // Reference a previously declared characteristic
 #define BT_SVC_READ_WRITE_VAR_CHRC_REFERENCE(_bt_service_class, _char_num, _desc) \
-    BT_SVC_READ_WRITE_NOTIFY_CHRC_REFERENCE( \
-        read_write_ ## _bt_service_class ## _char_num, \
-        _desc, \
-        _bt_service_class ## _char_num ## _ReadWrite::read, \
-        _bt_service_class ## _char_num ## _ReadWrite::write, \
-        _bt_service_class ## _char_num ## _ReadWrite::isActiveCccCfgChanged)
+    BT_SVC_READ_WRITE_NOTIFY_CHRC_REFERENCE(                                      \
+        read_write_##_bt_service_class##_char_num,                                \
+        _desc,                                                                    \
+        _bt_service_class##_char_num##_ReadWrite::read,                           \
+        _bt_service_class##_char_num##_ReadWrite::write,                          \
+        _bt_service_class##_char_num##_ReadWrite::isActiveCccCfgChanged)
