b1ff957b03a3435628893c3df815c428031aff8a
Stuart Alldritt <s.k.alldritt@gmail.com>
2026-04-30 00:35:05 +0000

    Migrate to new USB API

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -8,12 +8,12 @@ LOG_MODULE_REGISTER(storage);
 
 static FATFS fat_fs;
 
-/* Mount point "/NAND:" maps to the "NAND" disk registered by the zephyr,flash-disk
+/* Mount point "/NAND" maps to the "NAND" disk registered by the zephyr,flash-disk
  * driver (fat_partition on the MX25R64, offset 0x12E000, 4 MB).
  * CONFIG_FS_FATFS_MOUNT_MKFS=y auto-formats the partition on first boot. */
 static struct fs_mount_t fat_mnt = {
     .type      = FS_FATFS,
-    .mnt_point = "/NAND:",
+    .mnt_point = "/NAND",
     .fs_data   = &fat_fs,
     .storage_dev = (void*)FIXED_PARTITION_ID(fat_storage),
 };
